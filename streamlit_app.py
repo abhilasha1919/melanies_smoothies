e9f9bee7d5b3921e4a183cdd0a57f58f59e9d80b
abhilasha1919 <abhilasha.bangrania@barclays.com>
2024-06-26 18:36:48 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,8 +18,8 @@ ingredients_list = st.multiselect('Choose up to 5 ingredients:',my_dataframe,max
 if ingredients_list:
         ingredients_string = ''
 
-        for fruits_chosen in ingredients_list:
-                ingredients_string += fruits_chosen + ' '
+        for fruit_chosen in ingredients_list:
+                ingredients_string += fruit_chosen + ' '
                 st.subheader(fruit_chosen + 'Nutrition Information')
                 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
                 fv.df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
