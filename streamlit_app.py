a47216576e8d12e5e3c867c20b01f074aced1e46
abhilasha1919 <abhilasha.bangrania@barclays.com>
2024-06-26 18:33:51 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,18 +16,19 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 ingredients_list = st.multiselect('Choose up to 5 ingredients:',my_dataframe,max_selections=5)
 
 if ingredients_list:
-    ingredients_string = ''
+        ingredients_string = ''
 
-    for fruits_chosen in ingredients_list:
-        ingredients_string += fruits_chosen + ' '
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-        fv.df=st.dataframe(data=fruityvice_response.json(),use_container_width=True)
+        for fruits_chosen in ingredients_list:
+                ingredients_string += fruits_chosen + ' '
+                st.subheader(fruit_chosen + 'Nutrition Information')
+                fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen")
+                fv.df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
         
-        my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
-        values('""" + ingredients_string + """' , '"""+name_on_order+"""')"""
+                my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
+                values('""" + ingredients_string + """' , '"""+name_on_order+"""')"""
 
-    time_to_insert = st.button("Submit Order")
+        time_to_insert = st.button("Submit Order")
 
-    if time_to_insert:
-        session.sql(my_insert_stmt).collect()
-        st.success('Your Smoothie is ordered!', icon="✅")
+        if time_to_insert:
+            session.sql(my_insert_stmt).collect()
+            st.success('Your Smoothie is ordered!', icon="✅")
