7419b1733832efca8988f4d711a2b527631cb140
abhilasha1919 <abhilasha.bangrania@barclays.com>
2024-06-26 11:23:57 +0530

    Create streamlit_app.py
    copied from my Custome Smoothie SiS app

---
+++ streamlit_app.py
@@ -0,0 +1,31 @@
+# Import python packages
+import streamlit as st
+from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark.functions import col, when_matched
+
+# Write directly to the app 
+st.title(":cup_with_straw: Pending smoothie orders :cup_with_straw:")
+st.write("""Orders that need to be filled.""")
+
+session = get_active_session()
+my_dataframe = session.table("smoothies.public.orders").select(col('ORDER_FILLED'),col('NAME_ON_ORDER'),col('INGREDIENTS'),col('ORDER_UID'))
+
+if my_dataframe:
+    editable_df = st.data_editor(my_dataframe)
+    submitted = st.button('Submit')
+    if submitted:
+
+        og_dataset = session.table("smoothies.public.orders")
+        edited_dataset = session.create_dataframe(editable_df)
+
+    try:
+        og_dataset.merge(edited_dataset
+                     , (og_dataset['order_uid'] == edited_dataset['order_uid'])
+                     , [when_matched().update({'ORDER_FILLED': edited_dataset['ORDER_FILLED']})]
+                    )
+        st.success("Order(s) Updated!", icon="👍")
+    except:
+        st.write('Something went wrong.')
+
+else:
+    st.success("There are no pending orders right now",icon="👍")
